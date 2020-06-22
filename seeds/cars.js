fbb718ee21e47eeea5b0dc32bb6437d49e20fd2c
Darrell-Conklin <dmconklin84@gmail.com>
2020-06-22 06:41:56 -0500

    added seed data

---
+++ seeds/cars.js
@@ -0,0 +1,10 @@
+
+exports.seed = async function(knex) {
+  
+    await knex("cars").insert([
+        {vin: "1234QWER5678", make: "Toyota", model: "Corola", mileage: 50000.0, transmission: "manual", titleStatus: "clean"},
+        {vin: "9012ASDF3456", make: "Ford", model: "Mustang", mileage: 75000.6, transmission: "automatic", titleStatus: "clean"},
+        {vin: "7890ZXCV1234", make: "Chevy", model: "Impala", mileage: 150667.7, transmission: "automatic", titleStatus: "salvage"}
+    ])
+
+};
