22c45dccb8337cd6c8385191200ed261310cc359
Darrell-Conklin <dmconklin84@gmail.com>
2020-08-25 19:09:57 -0500

    changed server console log message

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./server/server')
 const PORT = process.env.port || 5000
 
 server.listen(PORT, () => {
-    console.log(`Server restarting\nServer listening on port ${PORT}`)
+    console.log(`Server listening on port ${PORT}`)
 })
